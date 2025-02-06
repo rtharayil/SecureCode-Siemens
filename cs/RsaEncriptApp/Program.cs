141a9bacaa1e3c18d39394b4c25351731019d241
rtharayil <ranjiththarayil@gmail.com>
2025-02-06 14:53:17 +0530

    added code  for cs

---
+++ cs/RsaEncriptApp/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string privateKeyPath = "private_key.xml";
+        string publicKeyPath = "public_key.xml";
+        string messagePath = "message.txt";
+        string encryptedMessagePath = "encrypted_message.bin";
+
+        // Generate RSA keys
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider(2048))
+        {
+            // Save private key
+            File.WriteAllText(privateKeyPath, rsa.ToXmlString(true));
+
+            // Save public key
+            File.WriteAllText(publicKeyPath, rsa.ToXmlString(false));
+
+            Console.WriteLine("Keys generated and saved.");
+        }
+
+        // Encrypt a message
+        string message = File.ReadAllText(messagePath);
+        byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+        byte[] encryptedMessage;
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.FromXmlString(File.ReadAllText(publicKeyPath));
+            encryptedMessage = rsa.Encrypt(messageBytes, false);
+        }
+
+        // Save the encrypted message
+        File.WriteAllBytes(encryptedMessagePath, encryptedMessage);
+        Console.WriteLine("Message encrypted and saved.");
+
+        // Decrypt the message
+        byte[] encryptedMessageFromFile = File.ReadAllBytes(encryptedMessagePath);
+
+        byte[] decryptedMessageBytes;
+        using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
+        {
+            rsa.FromXmlString(File.ReadAllText(privateKeyPath));
+            decryptedMessageBytes = rsa.Decrypt(encryptedMessageFromFile, false);
+        }
+
+        string decryptedMessage = Encoding.UTF8.GetString(decryptedMessageBytes);
+        Console.WriteLine("Decrypted Message:");
+        Console.WriteLine(decryptedMessage);
+    }
+}
---
+++ cs/RsaSignatureApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Create a new RSA key pair
+        using (RSA rsa = RSA.Create())
+        {
+            rsa.KeySize = 512;
+
+            // Generate public and private keys
+            RSAParameters publicKey = rsa.ExportParameters(false);
+            RSAParameters privateKey = rsa.ExportParameters(true);
+
+            // Message to sign
+            string message = "some message";
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+
+            // Sign the message using SHA1
+            byte[] signature = rsa.SignData(messageBytes, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
+
+            // Print signature in hex format
+            Console.WriteLine("Signature (Hex): " + BitConverter.ToString(signature).Replace("-", "").ToLower());
+
+            // Verify the message
+            bool isVerified = rsa.VerifyData(messageBytes, signature, HashAlgorithmName.SHA1, RSASignaturePadding.Pkcs1);
+
+            Console.WriteLine("Signature Verified: " + isVerified);
+        }
+    }
+}
